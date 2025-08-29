ab8859c1d497110bce359c1e78b622859b5db2f3
Leeza Shrestha <leezashrestha999@gmail.com>
2025-08-29 16:59:52 +0545

    Initial setup

---
+++ laravel_inertia_react/resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="p-6 text-center">
+            <h1 className="text-3xl text-red-700">Welcome to Inertia</h1>
+        </div>
+    );
+}
